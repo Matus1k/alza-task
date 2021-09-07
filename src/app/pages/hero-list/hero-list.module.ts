6fd0f38da8b39c7e6ad98a3a925eb2f13eaafe48
Matus1k <matus.posypanka3@gmail.com>
2021-09-07 04:44:21 +0200

    feat(hero-list-page): create hero list and chosen hero detail

--- src/app/pages/hero-list/hero-list.module.ts
+++ src/app/pages/hero-list/hero-list.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeroListPage } from './hero-list.page';
 import { RouterModule } from '@angular/router';
+import { TagModule } from '../../components/tag/tag.module';
 
 @NgModule({
   declarations: [HeroListPage],
   imports: [
     CommonModule,
+    TagModule,
     RouterModule.forChild([{ path: '', component: HeroListPage }]),
   ],
 })
--- src/app/pages/hero-list/hero-list.page.ts
+++ src/app/pages/hero-list/hero-list.page.ts
@@ -1,8 +1,15 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  OnDestroy,
+  OnInit,
+} from '@angular/core';
 import { HeroesService } from '../../services/heroes.service.ts/heroes.service';
-import { Observable } from 'rxjs';
+import { Subject } from 'rxjs';
 import { Hero } from '../../models/hero.model';
 import { Route } from '../../models/route.enum';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'alza-hero-list-page',
@@ -10,14 +17,35 @@ import { Route } from '../../models/route.enum';
   styleUrls: ['./hero-list.page.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HeroListPage implements OnInit {
-  heroes$: Observable<Hero[]>;
+export class HeroListPage implements OnInit, OnDestroy {
+  heroes: Hero[] = [];
+  chosenHero: Hero | undefined;
+
+  private readonly unsubscribe$ = new Subject();
+
   readonly Route = Route;
-  constructor(private heroesService: HeroesService) {
-    this.heroes$ = new Observable<Hero[]>();
-  }
+  constructor(
+    private readonly heroesService: HeroesService,
+    readonly ref: ChangeDetectorRef
+  ) {}
 
   ngOnInit(): void {
-    this.heroes$ = this.heroesService.getHeroes();
+    this.heroesService
+      .getHeroes()
+      .pipe(takeUntil(this.unsubscribe$.asObservable()))
+      .subscribe((heroes) => {
+        this.heroes = heroes;
+        this.chosenHero = heroes[0];
+        this.ref.markForCheck();
+      });
+  }
+
+  ngOnDestroy() {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
+  }
+
+  changeChosenHero(id: number) {
+    this.chosenHero = this.heroes.find((hero) => hero.id === id);
   }
 }
